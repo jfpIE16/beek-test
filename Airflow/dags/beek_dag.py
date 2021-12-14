f364f9f3a9b0f8234c358ab8cad524825eb0a6ae
Fernando Perez <jf23perez@gmail.com>
2021-12-14 00:15:54 -0600

    EDA Challenge finished, added ETL challenge definition

---
+++ Airflow/dags/beek_dag.py
@@ -0,0 +1,57 @@
+"""Example DAG demonstrating the usage of the BashOperator."""
+
+from datetime import datetime, timedelta
+import pytz
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+tz = pendulum.timezone('America/Argentina/Buenos_Aires')
+
+with DAG(
+    dag_id='beek_test_etl',
+    start_date=datetime(2021, 12, 1, tzinfo=tz),
+    schedule_interval='0 6 * * *',
+    catchup=False
+) as dag:
+    run_this_last = DummyOperator(
+        task_id='run_this_last',
+    )
+
+    # [START howto_operator_bash]
+    run_this = BashOperator(
+        task_id='run_after_loop',
+        bash_command='echo 1',
+    )
+    # [END howto_operator_bash]
+
+    run_this >> run_this_last
+
+    for i in range(3):
+        task = BashOperator(
+            task_id='runme_' + str(i),
+            bash_command='echo "{{ task_instance_key_str }}" && sleep 1',
+        )
+        task >> run_this
+
+    # [START howto_operator_bash_template]
+    also_run_this = BashOperator(
+        task_id='also_run_this',
+        bash_command='echo "run_id={{ run_id }} | dag_run={{ dag_run }}"',
+    )
+    # [END howto_operator_bash_template]
+    also_run_this >> run_this_last
+
+# [START howto_operator_bash_skip]
+this_will_skip = BashOperator(
+    task_id='this_will_skip',
+    bash_command='echo "hello world"; exit 99;',
+    dag=dag,
+)
+# [END howto_operator_bash_skip]
+this_will_skip >> run_this_last
+
+if __name__ == "__main__":
+    dag.cli()
---
+++ ETL_Challenge.md
@@ -0,0 +1,7 @@
+# ETL Challenge
+1. Model a table that shows us the data from when a user signs up to when a user pays for a subscription. Think about the business cases and model the data to be consistent with the business.
+2. Build an ETL that takes the information from those tables and makes a CSV file with the resulting table defined above.
+    * The ETL must run at 06:00 GMT -3.
+    * In case of failure of the ETL, have to send an email.
+    * Propose tests that validate the consistency of the data and how they will be implemented.
+3. The ETL code will be reviewed together in an onsite interview scheduled with our data team.
